4967476678c006f67d5013841b7152ef08562434
sdmitryTests <dssamsonov@miem.hse.ru>
2021-10-02 12:04:11 +0300

    Train

--- src/main/java/hse/projects/taskone/IO/FileIO.java
+++ src/main/java/hse/projects/taskone/IO/FileIO.java
@@ -8,26 +8,22 @@ import java.io.*;
 import java.util.List;
 
 public class FileIO {
-    public void write(String filePath, Street street, boolean append) {
-        try {
-            StreetSerializer streetSerializer = new StreetSerializer();
-            FileOutputStream jsonObjectWriter = new FileOutputStream(filePath, append);
-            jsonObjectWriter.write(streetSerializer.toJson(street).getBytes());
-            jsonObjectWriter.write("\n".getBytes());
-        } catch (IOException e) {
-            System.out.println("File error: cannot write an object in the file");
-        }
+    public void write(String filePath, Street street, boolean append) throws IOException {
+        StreetSerializer streetSerializer = new StreetSerializer();
+        FileOutputStream jsonObjectWriter = new FileOutputStream(filePath, append);
+        jsonObjectWriter.write(streetSerializer.toJson(street).getBytes());
+        jsonObjectWriter.write("\n".getBytes());
     }
 
-    public List<Street> read(String filePath) throws Exception {
+    public List<Street> read(String filePath) throws IOException {
         StreetDeserializer streetDeserializer = new StreetDeserializer();
         StringBuilder jsonObject = new StringBuilder();
         File file = new File(filePath);
         FileInputStream jsonObjectReader = new FileInputStream(file);
         BufferedInputStream bufferedInputStream = new BufferedInputStream(jsonObjectReader, 200);
         int ch;
-        while ((ch=bufferedInputStream.read()) != -1) {
-            jsonObject.append((char)ch);
+        while ((ch = bufferedInputStream.read()) != -1) {
+            jsonObject.append((char) ch);
         }
         return streetDeserializer.fromJsonList(jsonObject.toString().replaceAll("\n", ""));
     }
